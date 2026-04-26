0ab3cc12236cc057cf110f7fdcba4299680f731c
Adam Patterson <adpatter@umich.edu>
2026-04-26 10:53:41 -0400

    Improve tests.

--- tests/scratch/src/main.ts
+++ tests/scratch/src/main.ts
@@ -3,7 +3,7 @@ import { Client, LockManager } from "@far-analytics/persistence";
 
 const manager = new LockManager({ errorHandler: () => {} });
 const client = new Client({ manager });
-const writeStream = await client.createWriteStream("/home/null/workspace/repos/far-analytics/persistence/tests/example/example.json");
+const writeStream = await client.createWriteStream("/home/null/workspace/repos/far-analytics/persistence/tests/scratch/scratch.json");
 writeStream.write(JSON.stringify({ message: "Streaming Hello, World!" }) + `\n`);
 writeStream.end();
 await once(writeStream, "finish");
