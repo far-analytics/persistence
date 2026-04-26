b813213e5c8af5c8e2bc14ed8ba6605b658085d6
Adam Patterson <adpatter@umich.edu>
2026-04-26 09:45:34 -0400

    Add exports.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import { WriteStreamOptions, ReadStreamOptions } from "./types.js";
+import { WriteStream } from "./write_stream.js";
 import { LockManager, LockManagerOptions, Artifacts, GraphNode } from "./lock_manager.js";
 import { Client } from "./client.js";
 
-export { Client, LockManager, LockManagerOptions, Artifacts, GraphNode };
+export { Client, LockManager, LockManagerOptions, Artifacts, GraphNode, WriteStream, WriteStreamOptions, ReadStreamOptions };
