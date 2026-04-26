085e825e6ae510cc2ecb2d3b288275da54152d79
Adam Patterson <adpatter@umich.edu>
2026-04-26 15:49:08 -0400

    Improve types.

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,10 @@
+import { Abortable } from "node:events";
 import * as fs from "node:fs";
 
-export type ReadStreamOptions = Omit<fs.ReadStreamOptions, "fd" | "autoClose" | "fs" | "flush">;
-export type WriteStreamOptions = Omit<fs.WriteStreamOptions, "fd" | "autoClose" | "fs" | "flush">;
+export type ReadStreamOptions = Omit<fs.ReadStreamOptions, "fd" | "autoClose" | "fs" | "flush" | "emitClose">;
+export type WriteStreamOptions = Omit<fs.WriteStreamOptions, "fd" | "autoClose" | "fs" | "flush" | "emitClose">;
+export type WriteFileOptions = fs.ObjectEncodingOptions &
+  Abortable & {
+    mode?: fs.Mode | undefined;
+    flag?: string | undefined;
+  };
